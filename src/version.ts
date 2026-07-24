75c163bb08793be1dd81b61386fe89fb69f8be94
Angel Ramirez <angel@example.com>
2026-07-24 04:19:46 -0600

    feat: pwa versioning con UpdateNotification + endpoint app-version

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // Auto-generated - do not edit
-export const VERSION = '0.1.47';
-export const BUILD_HASH = '1897847';
+export const VERSION = '0.1.48';
+export const BUILD_HASH = '2dee2cc';
 export const BUILD_BRANCH = 'main';
 export const BUILD_TIME = '2026-07-24T07:04:36.861Z';
 export const COMMIT_TIME = '2026-07-24T01:00:48-06:00';
