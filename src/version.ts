79020dac9623b8889f8c88f7f4c451dfb06b0a5f
Angel Ramirez <angel@example.com>
2026-07-24 01:05:29 -0600

    chore: sync version 0.1.94

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // Auto-generated - do not edit
-export const VERSION = '0.1.44';
-export const BUILD_HASH = '52153bd';
+export const VERSION = '0.1.94';
+export const BUILD_HASH = '22e3993';
 export const BUILD_BRANCH = 'main';
-export const BUILD_TIME = '2026-07-24T06:42:51.172Z';
-export const COMMIT_TIME = '2026-07-24T00:35:56-06:00';
+export const BUILD_TIME = '2026-07-24T07:04:36.861Z';
+export const COMMIT_TIME = '2026-07-24T01:00:48-06:00';
