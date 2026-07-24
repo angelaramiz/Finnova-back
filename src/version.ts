f09c1fa979a88e0b0d6de10ea889f14f2feab9b8
Angel Ramirez <angel@example.com>
2026-07-24 00:42:58 -0600

    chore: bump version to 0.1.44

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // Auto-generated - do not edit
-export const VERSION = '0.1.43';
-export const BUILD_HASH = '6b5d7b3';
+export const VERSION = '0.1.44';
+export const BUILD_HASH = '52153bd';
 export const BUILD_BRANCH = 'main';
-export const BUILD_TIME = '2026-07-24T06:35:50.757Z';
-export const COMMIT_TIME = '2026-07-24T00:34:52-06:00';
+export const BUILD_TIME = '2026-07-24T06:42:51.172Z';
+export const COMMIT_TIME = '2026-07-24T00:35:56-06:00';
